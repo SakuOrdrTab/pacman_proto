9e6ca01ac58dc8e0ce9d09bc40ed081095d8c1f6
SakuOrdrTab <leinosensaku@gmail.com>
2024-04-05 22:00:02 +0300

    revised documenttation, method names changed from Qt style to pythonic

--- pacman.py
+++ pacman.py
@@ -1,4 +1,4 @@
-# Pac-man representation time counter
+''' Pac-man representation time counter '''
 import sys
 
 from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QLineEdit, QPushButton, QDialog, QDialogButtonBox
@@ -19,32 +19,30 @@ class PacmanWindow(QWidget):
             self._animation_duration = self._minutes if self._minutes > 0 else 0.01 # ensure non negative time
 
         animation_start_delay_ms = (self._minutes - self._animation_duration) * 60 * 1000
-        print(f"Animation start delay: {animation_start_delay_ms} ms")
         
-        # Window setup...
-        self.setupWindow()
+        # Window setup
+        self.setup_window()
 
         # Delay starting the animations
-        QTimer.singleShot(animation_start_delay_ms, self.startAnimations)
+        QTimer.singleShot(animation_start_delay_ms, self.start_animations)
 
-    def setupWindow(self):
+    def setup_window(self):
         """Set up the window and load UI components immediately."""
         # Set the window to be always on top
         self.setWindowFlag(Qt.WindowStaysOnTopHint, True)
         self.setWindowFlag(Qt.FramelessWindowHint, True)
         # Set the window size to the screen size
         display_dimensions = QScreen.availableGeometry(QGuiApplication.primaryScreen())
-        print(display_dimensions)
         # Set the height of the window to 1/16 of the screen height
         display_dimensions.setHeight(display_dimensions.height() // 16)
         self.setGeometry(display_dimensions)
         # Make the window transparent
         self.setAttribute(Qt.WA_TranslucentBackground)
-        # Set window flags and geometry...
+        # Set window flags and geometry
         self.setWindowTitle('Pacman representation time guardian')
 
         # Load pacman pictures and set up QLabel for Pac-Man...
-        self.loadPacmanPictures()
+        self.load_pacman_pictures()
 
         # Save the window size as variables
         self._screen_width = self.width()  # Get the current width of the window
@@ -52,7 +50,7 @@ class PacmanWindow(QWidget):
         self._screen_height = self.height()  # Get the current height of the window
         print(self._screen_height)
 
-    def loadPacmanPictures(self):
+    def load_pacman_pictures(self):
         """Loads the pacman pictures and prepares the QLabel."""
         # Load pacman pictures...
         self.pacmanOpen = QPixmap('pacman_mouth_open.png').scaled(self.height(), self.height())
@@ -60,12 +58,12 @@ class PacmanWindow(QWidget):
         # Load end animation image, don't resize yet
         self.pacman_face = QPixmap('pacman_face.png')
 
-        # Save also the pacman size for more readable code...
+        # Save also the pacman size for more readable code
         self._pacman_size = self.pacmanOpen.height()
         # Save the state of the pacman's mouth
         self._mouth_open = True
 
-        # Create a QLabel widget for Pac-Man...
+        # Create a QLabel widget for Pac-Man
         self.pacmanLabel = QLabel(self)
         self.pacmanLabel.setPixmap(self.pacmanOpen)
         self.pacmanLabel.resize(self.pacmanOpen.size())
@@ -73,38 +71,38 @@ class PacmanWindow(QWidget):
         # Initially hide the Pac-Man QLabel
         self.pacmanLabel.hide()
     
-    def startAnimations(self):
+    def start_animations(self):
         """Starts the moving and mouth animations for Pac-Man."""
         self.pacmanLabel.show()
-        self.startMovingAnimation()
-        self.startMouthAnimation()
+        self.start_moving_animation()
+        self.start_mouth_animation()
 
-    def startMovingAnimation(self):
+    def start_moving_animation(self):
         """Starts the pacman's moving animation. The pacman will move from the left to the right of the screen.
         """        
         #  Ensure the entire size of the Pac-Man image is accommodated
         startRect = QRect(0, self._screen_height // 2 - self._pacman_size // 2, self._pacman_size, self._pacman_size)  # Start at the left, centered vertically
         endRect = QRect(self._screen_width, self._screen_height // 2 - self._pacman_size // 2, self._pacman_size, self._pacman_size)  # End at the right, centered vertically
 
-
-        self.movingAnimation = QPropertyAnimation(self.pacmanLabel, b"geometry")
-        self.movingAnimation.setDuration(self._animation_duration * 60 *1000) # Set pacman animation duration
-        self.movingAnimation.setStartValue(startRect)
-        self.movingAnimation.setEndValue(endRect)
-        self.movingAnimation.setLoopCount(1)  # -1 for infinite loop
+        # Create the animation object
+        self.moving_animation = QPropertyAnimation(self.pacmanLabel, b"geometry")
+        self.moving_animation.setDuration(self._animation_duration * 60 *1000) # Set pacman animation duration
+        self.moving_animation.setStartValue(startRect)
+        self.moving_animation.setEndValue(endRect)
+        self.moving_animation.setLoopCount(1)  # -1 for infinite loop
         # Connect the finished signal to the end_of_time method
-        self.movingAnimation.finished.connect(self.end_of_time)
+        self.moving_animation.finished.connect(self.end_of_time)
 
-        self.movingAnimation.start()
+        self.moving_animation.start()
 
-    def startMouthAnimation(self):
+    def start_mouth_animation(self):
         """Starts the pacman's mouth animation. The mouth will open and close every 500 ms.
         """        
         self.mouthTimer = QTimer(self)
-        self.mouthTimer.timeout.connect(self.toggleMouth)
+        self.mouthTimer.timeout.connect(self.toggle_mouth)
         self.mouthTimer.start(500)  # Adjust the interval for faster/slower mouth animation
 
-    def toggleMouth(self):
+    def toggle_mouth(self):
         """Toggles the pic of pacman: open or closed mouth. State is saved in the attribute self._mouth_open.
         """
         if self._mouth_open:
